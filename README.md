68225f6d8fef7e7dee52bb43073c7dd799ec7bf5
tony <developedbyant@gmail.com>
2022-10-24 11:23:25 -0400

    Made function smaller and faster

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # Simple node.js scheduler
 Node.js scheduler using while loop and await
-```javascript
+### Scheduler takes a prop
+```typescript
+interface Props{
+    /** Run at given hour and minute */
+    runAt:{ hour:0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23,minute:number } | { hour:-1,minute:-1 }
+    /** (Milliseconds) run function every given time */
+    every:number
+    /** Do not run on given week days (1=monday) */
+    skipDays?:number[]
+    /** Tell function to run passed func first or after wait(every) time */
+    runFuncFirst?:boolean
+    /** Function to run */
+    func:Function
+    /** TimeZone */
+    TZ?:string
+}
+```
+### Import module
+```typeScript
+import scheduler from "@developedbyant/scheduler"
+```
+### Examples of how to use
+```typeScript
 // Import function
-import  scheduler from "@developedbyant/scheduler"
+import scheduler from "@developedbyant/scheduler"
 
 // Run every|24hours (1440000)
 // At 9:30am and skip saturday and sunday
@@ -10,7 +32,7 @@ scheduler({
     runAt:{ hour:9, minute:30 },
     every:1440000,
     skipDays:[6,7],
-    func:()=>{ 
+    func:()=> { 
         console.log("Running function")
     }
 })
@@ -28,4 +50,19 @@ scheduler({
     }
 })
 
+// Run function right alway every 5 seconds
+// Set runFuncFirst to run function and wait for prop.every after
+scheduler({
+    // Set { hour:-1, minute:-1 } to run right alway
+    runAt:{ hour:-1, minute:-1 },
+    // Set runFuncFirst to run function and wait for prop.every after
+    runFuncFirst:true,
+    // 5 Seconds = 5000 milliseconds
+    every:5000,
+    // Do not skip any day
+    skipDays:[],
+    func:()=>{ 
+        console.log("Running function")
+    }
+})
 ```
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
+/** Get string day from getDay() index */
+const DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
 interface Props{
-    /** Run on given Date */
-    runAt:{ hour:1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23,minute:number } | { hour:-1,minute:-1 }
-    /** (Miliseconds) run function every given time */
+    /** Run at given hour and minute */
+    runAt:{ hour:0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23,minute:number } | { hour:-1,minute:-1 }
+    /** (Milliseconds) run function every given time */
     every:number
-    /** Do not run on given week day (0=monday) */
+    /** Do not run on given week days (1=monday) */
     skipDays?:number[]
-    /** TimeZone */
-    TZ?:string
+    /** Tell function to run passed func first or after wait(every) time */
+    runFuncFirst?:boolean
     /** Function to run */
     func:Function
+    /** TimeZone */
+    TZ?:string
 }
 
-/** Get next day Date */
-function getNextDay(){
-    const date = new Date()
-    date.setHours(0,0,0,0)
-    date.setDate(date.getDate()+1)
-    return date
-}
 /** Utility to wait */
 async function wait(time:number) {
     return new Promise(r=>setTimeout(r,time))
@@ -35,39 +33,39 @@ export default async function scheduler(props:Props){
     // Schedule run at time
     if(props.runAt.hour!==-1&&props.runAt.minute!==-1){
         const currentDate = new Date()
-        const currentHour = currentDate.getHours()
-        const currentMinute = currentDate.getMinutes()
-        const waitTimeDate = new Date()
-        // Set given hour and minute
-        waitTimeDate.setHours( props.runAt.hour,props.runAt.minute,0,0)
-        // If  given hour is lower than current hour schudule for next day
-        if((props.runAt.hour<currentHour) || (props.runAt.hour===currentHour && props.runAt.minute<currentMinute)) waitTimeDate.setDate(currentDate.getDate()+1)
-        // Set waitTime props.waitTime
-        // Sub future time with current time to get time between
-        console.log(`Function will run on:${waitTimeDate.toString()}`)
-        // Wait given waitting time
-        await wait(waitTimeDate.getTime() - currentDate.getTime())
+        const runAtDate = new Date()
+        runAtDate.setHours( props.runAt.hour,props.runAt.minute,0,0)
+        // If runAt date is lower than current date, schedule for next day
+        if(runAtDate < currentDate) runAtDate.setDate(runAtDate.getDate()+1)
+        // Else schedule for same day
+        console.log(`Function will try run on:${runAtDate.toString()}`)
+        // Wait given waiting time
+        await wait(runAtDate.getTime() - currentDate.getTime())
     }
 
     /** Date that function started, it's use when skipping week days */
-    let startDate = new Date()
-    // Run function every given miniseconds
+    const startedATDate = new Date()
+    // Run function every given milliseconds
     while(true){
-        // Skip days, this will allow to wait to run the function next day
-        // At the same time it was started at
+        // Check for any skip days
         if(props.skipDays!==undefined && props.skipDays.includes(new Date().getDay())){
-            // Get for next day
-            const nextDay = getNextDay()
-            const reRunDate = nextDay.setHours(startDate.getHours(),startDate.getMinutes(),startDate.getSeconds(),startDate.getMilliseconds())
-            // Update startDate
-            startDate = new Date(reRunDate)
-            console.log(`SkipDays founded:${props.skipDays.join(",")}, we will try to run on:${new Date(reRunDate).toString()}`)
+            // Get for next day and Update started at date
+            startedATDate.setDate(startedATDate.getDate()+1)
+            console.log(`Founded skip days: ${props.skipDays.map((d,i)=>DAYS[i]).join(",")}\nWe will try to run on:${startedATDate.toString()}`)            
             // Wait for the time between now and re run date
-            await wait(reRunDate - new Date().getTime())
+            await wait(startedATDate.getTime() - new Date().getTime())
         }
-        // Run function
-        props.func()
-        // Wait for given props.every
-        await wait(props.every)
+        // Check what needs to run first, function or wait time
+        if(props.runFuncFirst){
+            // Run function
+            props.func()
+            // Wait for given props.every
+            await wait(props.every)
+        }else{
+            // Wait for given props.every
+            await wait(props.every)
+            // Run function
+            props.func()
+        }
     }
 }
